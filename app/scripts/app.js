9d702a46cc82786062284dce8ca8581b9c317cf4
ruby -v <rails -c>
2017-07-16 04:16:21 -0700

    YV

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,14 @@
-angular.module('blocJams', []);
+(function() {
+     function config($stateProvider, $locationProvider) {
+          $locationProvider
+         .html5Mode({
+             enabled: true,
+             requireBase: false
+         });
+     }
+ 
+ angular.module('blocJams', ['ui.router']);
+     angular
+         .module('blocJams', ['ui.router'])
+         .config(config);
+ })();
