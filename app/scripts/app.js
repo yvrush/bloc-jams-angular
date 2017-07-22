c4f772ad00a350f40a877be5f815a705d9015174
ruby -v <rails -c>
2017-07-22 01:17:02 -0700

    YVFriday checkpoint

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,11 +8,14 @@
         $stateProvider
             .state('landing', {
                 url: '/',
+                controller: 'LandingCtrl as landing',
                 templateUrl: '/templates/landing.html'
             })
             .state('collection', {
                 url: '/collection',
+                controller: 'CollectionCtrl as collection',
                 templateUrl: '/templates/collection.html'
+
             })
             .state('album', {
                 url: '/album',
---
+++ app/scripts/fixtures.js
@@ -0,0 +1,74 @@
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Blue', duration: '4:26', audioUrl: 'assets/music/blue' },
+         { title: 'Green', duration: '3:14', audioUrl: 'assets/music/green' },
+         { title: 'Red', duration: '5:01', audioUrl: 'assets/music/red' },
+         { title: 'Pink', duration: '3:21', audioUrl: 'assets/music/pink' },
+         { title: 'Magenta', duration: '2:15', audioUrl: 'assets/music/magenta' }
+     ]
+};
+
+
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        {
+            title: 'Hello, Operator?',
+            duration: '1:01'
+        },
+        {
+            title: 'Ring, ring, ring',
+            duration: '5:01'
+        }, {
+            title: 'Fits in your pocket',
+            duration: '3:21'
+        },
+        {
+            title: 'Can you hear me now?',
+            duration: '3:14'
+        },
+        {
+            title: 'Wrong phone number',
+            duration: '2:15'
+        }
+     ]
+};
+
+var albumMozart = {
+    title: '32Masterpieces',
+    artist: 'Mozart',
+    label: 'Classic',
+    year: '2007',
+    albumArtUrl: 'assets/images/album_covers/03.png',
+    songs: [
+        {
+            title: 'Serenade in G',
+            duration: '4:26'
+        },
+        {
+            title: 'Piano concerto',
+            duration: '3:14'
+        },
+        {
+            title: 'Clarinet quintet',
+            duration: '5:01'
+        },
+        {
+            title: 'Vesperae solennes',
+            duration: '3:21'
+        },
+        {
+            title: 'Le noxxr di figaro',
+            duration: '2:15'
+        }
+            ]
+};
