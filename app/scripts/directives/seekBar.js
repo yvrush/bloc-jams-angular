8f055213181ec1f33f4a7f5bb9588733ed225aec
Yury <yury.verush@gmail.com>
2017-09-25 18:57:30 -0700

    Pre 10 commit

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -1,7 +1,7 @@
-(function() {
-    function seekBar($document)  {
+(function () {
+    function seekBar($document) {
 
-        var calculatePercent = function(seekBar, event){
+        var calculatePercent = function (seekBar, event) {
             var offsetX = event.pageX - seekBar.offset().left;
             var seekBarWidth = seekBar.width();
             var offsetXPercent = offsetX / seekBarWidth;
@@ -14,46 +14,51 @@
             templateUrl: '/templates/directives/seek_bar.html',
             replace: true,
             restrict: 'E',
-            scope: { },
-            link: function(scope, element, attributes){
+            scope: {},
+            link: function (scope, element, attributes) {
                 scope.value = 0;
                 scope.max = 100;
 
                 var seekBar = $(element);
                 /*
-                * @function percentString
-                * converts percent number to string type
-                */
+                 * @function percentString
+                 * converts percent number to string type
+                 */
 
-                var percentString = function() {
+                var percentString = function () {
                     var value = scope.value;
                     var max = scope.max;
                     var percent = value / max * 100;
                     return percent + "%";
                 };
 
-                scope.fillStyle = function() {
-                    return {width: percentString()};
+                scope.fillStyle = function () {
+                    return {
+                        width: percentString()
+                    };
                 };
 
-                scope.thumbStyle = function() {
-                        return {left: percentString()};
+
+                scope.thumbStyle = function () {
+                    return {
+                        left: percentString()
+                    };
                 };
 
-                scope.onClickSeekBar = function(event) {
+                scope.onClickSeekBar = function (event) {
                     var percent = calculatePercent(seekBar, event);
                     scope.value = percent * scope.max;
                 };
 
-                scope.trackThumb = function() {
-                    $document.bind('mousemove.thumb', function(event){
+                scope.trackThumb = function () {
+                    $document.bind('mousemove.thumb', function (event) {
                         var percent = calculatePercent(seekBar, event);
-                        scope.$appy(function(){
+                        scope.$apply(function () {
                             scope.value = percent * scope.max;
                         });
                     });
 
-                    $document.bind('mouseup.thumb', function(){
+                    $document.bind('mouseup.thumb', function () {
                         $document.unbind('mousemouve.thumb');
                         $document.unbind('mouseup.thumb');
                     });
